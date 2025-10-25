225d4790e58a666e4f7374e98d3537446b9652ba
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-10-25 23:06:14 +0530

    added product client

---
+++ src/api/api_clients/product_client.py
@@ -0,0 +1,11 @@
+from src.api.api_clients.base_client import _BaseClient
+from src.common.log_utils import log
+
+
+class ProductClient(_BaseClient):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    @log
+    def get_products(self):
+        return self.get(endpoint=self.endpoints["product"]["products"])
